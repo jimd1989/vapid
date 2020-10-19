e66d1b604e92d57033e964e48c0b80d985d5e3c8
jimd1989 <jamesdalrymple@protonmail.com>
2020-10-19 12:43:01 +1100

    Automated path resolution

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+path="'$PWD/src/vue.js';"
+import="import Vue from $path"
 cat "src/items.ts" > annealing.ts
 cat "src/constraints-parsing.ts" >> annealing.ts
 cat "src/constraints.ts" >> annealing.ts
