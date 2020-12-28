1eefb6b355eb621ea8ae5a0b3a8b4ce077ccc0d3
jimd1989 <jamesdalrymple@protonmail.com>
2020-12-28 18:04:12 +1000

    text input

--- src/Main.purs
+++ src/Main.purs
@@ -3,49 +3,35 @@ module Main where
 import Prelude
 
 import Data.Maybe (Maybe(..))
+import Data.Symbol (SProxy(..))
 import Effect (Effect)
+import Effect.Class (class MonadEffect)
+import Effect.Class.Console (logShow)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
 
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  runUI xomponent unit body
+  runUI component unit body
 
-data Action = Increment | Decrement
-
-xomponent = H.mkComponent
-              { initialState,
-                render,
-                eval: H.mkEval $ H.defaultEval { handleAction = f } }
+component ∷ ∀ q i o m. H.Component HH.HTML q i o m
+component =
+  H.mkComponent
+    { initialState,
+      render,
+      eval: H.mkEval $ H.defaultEval { handleAction = f }
+    }
   where
-  initialState = const ["a"]
-  render state = HH.div_ [HH.text $ show state, 
-                         HH.button [ HE.onClick $ const $ Just Increment] [HH.text "+"]]
-  f = case _ of
-    Increment -> H.modify_ \state → state <> ["a"]
-    _         -> H.modify_ identity
-
--- component =
---   H.mkComponent
---     { initialState
---     , render
---     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
---     }
---   where
---   initialState _ = 0
--- 
---   render state =
---     HH.div_
---       [ HH.button [ HE.onClick \_ -> Just Decrement ] [ HH.text "-" ]
---       , HH.div_ [ HH.text $ show state ]
---       , HH.button [ HE.onClick \_ -> Just Increment ] [ HH.text "+" ]
---       , HH.ul_ [ HH.li_ [HH.text "ass" ]]
---       ]
--- 
---   handleAction = case _ of
---     Increment -> H.modify_ \state -> state + 1
---     Decrement -> H.modify_ \state -> state - 1
+  initialState ∷ ∀ i. i → { name ∷ Maybe String}
+  initialState = const { name: Nothing }
+  render ∷ ∀ m. { name ∷ Maybe String } → H.ComponentHTML String () m
+  render {name: Nothing}  = HH.div_ [HH.p_ [HH.text "Nothing yet." ], HH.input [HE.onValueInput pure ]]
+  render {name: Just ""}  = HH.div_ [HH.p_ [HH.text "Nothing yet." ], HH.input [HE.onValueInput pure ]]
+  render {name: Just x }  = HH.div_ [HH.p_ [HH.text x], HH.input [HE.onValueInput pure ]]
+  f ∷ ∀ o m. String → H.HalogenM { name ∷ Maybe String} String () o m Unit
+  f x = H.modify_ \state → { name: Just x }
