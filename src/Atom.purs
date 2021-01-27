ab68fd24464004d8baf8cd17d295531cef59a56e
jimd1989 <jamesdalrymple@protonmail.com>
2021-01-28 08:52:43 +1000

    parent/child compiles: needs extra wiring

--- src/Atom.purs
+++ src/Atom.purs
@@ -1,52 +1,50 @@
 module Atom where
 
-import Prelude (($), identity, pure, show)
+import Prelude (($), pure, show)
 import Data.Const (Const)
 import Data.Function (const)
 import Data.Int (fromString)
 import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
-import Data.Unit (Unit, unit)
-import Data.Void (Void, absurd)
+import Data.Unit (Unit)
+import Data.Void (Void)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Helpers ((∘), class', component')
 
-
 type Label = String
-type LabelState = { label ∷ Label }
-data LabelAction = LabelModify Label
+labelSym = SProxy ∷ SProxy "label"
 
-label ∷ ∀ m. H.Component HH.HTML (Const Void) Unit Void m
+label ∷ ∀ m. H.Component HH.HTML (Const Void) Label Label m
 label = component' i r a
   where
-    i = const { label: "" }
-    a (LabelModify α) = H.modify_ _{ label = α }
-    r = const $ HH.input [class' "atomLabel", HE.onValueInput parse]
-    parse = pure ∘ LabelModify
-   
-type Magnitude = Int
-type MagnitudeState = { magnitude ∷ Maybe Magnitude }
-data MagnitudeAction = MagnitudeModify (Maybe Magnitude)
+    i = const ""
+    a = H.raise
+    r = const $ HH.input [class' "atomLabel", HE.onValueInput pure]
 
-magnitude ∷ ∀ m. H.Component HH.HTML (Const Void) Unit Void m
+type Magnitude = Maybe Int
+magnitudeSym = SProxy ∷ SProxy "magnitude"
+
+magnitude ∷ ∀ m. H.Component HH.HTML (Const Void) Magnitude Magnitude m
 magnitude = component' i r a
   where
-    i = const { magnitude: Just 1 }
-    a (MagnitudeModify n) = H.modify_ _{ magnitude = n }
-    r { magnitude: Nothing } =  HH.input [class' "magnitudeError", HP.placeholder "integer", HE.onValueInput parse]
-    r { magnitude: Just α } = HH.input [class' "magnitude", HP.placeholder $ show α, HE.onValueInput parse]
-    parse = pure ∘ MagnitudeModify ∘ fromString
+    i = const $ Just 1
+    a = H.raise
+    r Nothing  =  HH.input [class' "magnitudeError", HP.placeholder "integer", HE.onValueInput parse]
+    r (Just α) = HH.input [class' "magnitude", HP.placeholder $ show α, HE.onValueInput parse]
+    parse = pure ∘ fromString
 
-sym = SProxy ∷ SProxy "sym"
-lSlot = HH.slot sym 0 label unit absurd
-rSlot = HH.slot sym 1 magnitude unit absurd
+type AttributeState = { label ∷ Label, magnitude ∷ Magnitude }
+data AttributeAction = Label Label | Magnitude Magnitude
 
 attribute ∷ ∀ m. H.Component HH.HTML (Const Void) Unit Void m
 attribute = component' i r a
-  where
-    i = const { label: "" }
-    a (LabelModify α) = H.modify_ _{ label = α }
-    r = const $ HH.span [class' "attribute"] [lSlot, rSlot]
+ where
+   i = const { label: "", magnitude: Nothing }
+   a (Magnitude α) = H.modify_ _{ magnitude = α }
+   a (Label α)     = H.modify_ _{ label = α }
+   r α = HH.li [class' "attribute"] [labelSlot α, magnitudeSlot α]
+   labelSlot α     = HH.slot labelSym 0 label α.label (pure ∘ Label)
+   magnitudeSlot α = HH.slot magnitudeSym 1 magnitude α.magnitude (pure ∘ Magnitude)
