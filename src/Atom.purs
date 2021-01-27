ad179c57afa0001f5ea37c2651ccd8df0209feb7
jimd1989 <jamesdalrymple@protonmail.com>
2021-01-27 15:32:20 +1000

    label component

--- src/Atom.purs
+++ src/Atom.purs
@@ -13,28 +13,28 @@ import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Helpers ((∘), class', component')
 
+
+type Label = String
+type LabelState = { label ∷ Label }
+data LabelAction = LabelModify Label
+
+label ∷ ∀ m. H.Component HH.HTML (Const Void) Unit Void m
+label = component' i r a
+  where
+    i = const { label: "" }
+    a (LabelModify α) = H.modify_ _{ label = α }
+    r = const $ HH.input [class' "magnitude", HE.onValueInput parse]
+    parse = pure ∘ LabelModify
+   
 type Magnitude = Int
 type MagnitudeState = { magnitude ∷ Maybe Magnitude }
 data MagnitudeAction = MagnitudeModify (Maybe Magnitude)
-
-type Label = String
-data Attribute = Attribute { label ∷ Label, magnitude ∷ Magnitude }
 
 magnitude ∷ ∀ m. H.Component HH.HTML (Const Void) Unit Void m
 magnitude = component' i r a
   where
     i = const { magnitude: Just 1 }
     a (MagnitudeModify n) = H.modify_ _{ magnitude = n }
-    r { magnitude: Nothing } = 
-      HH.span [class' "magnitudeBox"]
-       [HH.input [class' "magnitudeError", HP.placeholder "ERROR!", HE.onValueChange parse]]
-    r { magnitude: Just α } =
-      HH.span [class' "magnitudeBox"]
-       [HH.input [class' "magnitude", HP.placeholder $ show α, HE.onValueChange parse]]
+    r { magnitude: Nothing } =  HH.input [class' "magnitudeError", HP.placeholder "integer", HE.onValueInput parse]
+    r { magnitude: Just α } = HH.input [class' "magnitude", HP.placeholder $ show α, HE.onValueInput parse]
     parse = pure ∘ MagnitudeModify ∘ fromString
-
---label ∷ ∀ w i. String → HH.HTML w i
---label α = HH.span [HP.class_ $ HH.ClassName "labelBox"] [HH.input [HP.class_ $ HH.ClassName "label"]]
-
---atom ∷ ∀ w i. HH.HTML w i
---atom = HH.li [HP.class_ $ HH.ClassName "atom"] [label "a"]
