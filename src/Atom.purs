a8c83199e1da55566d21b35065762cd42692b68f
jimd1989 <jamesdalrymple@protonmail.com>
2021-01-27 19:29:36 +1000

    basic component slots

--- src/Atom.purs
+++ src/Atom.purs
@@ -1,12 +1,13 @@
 module Atom where
 
-import Prelude (($), pure, show)
+import Prelude (($), identity, pure, show)
 import Data.Const (Const)
 import Data.Function (const)
 import Data.Int (fromString)
 import Data.Maybe (Maybe(..))
-import Data.Unit (Unit)
-import Data.Void (Void)
+import Data.Symbol (SProxy(..))
+import Data.Unit (Unit, unit)
+import Data.Void (Void, absurd)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
@@ -23,7 +24,7 @@ label = component' i r a
   where
     i = const { label: "" }
     a (LabelModify α) = H.modify_ _{ label = α }
-    r = const $ HH.input [class' "magnitude", HE.onValueInput parse]
+    r = const $ HH.input [class' "atomLabel", HE.onValueInput parse]
     parse = pure ∘ LabelModify
    
 type Magnitude = Int
@@ -38,3 +39,14 @@ magnitude = component' i r a
     r { magnitude: Nothing } =  HH.input [class' "magnitudeError", HP.placeholder "integer", HE.onValueInput parse]
     r { magnitude: Just α } = HH.input [class' "magnitude", HP.placeholder $ show α, HE.onValueInput parse]
     parse = pure ∘ MagnitudeModify ∘ fromString
+
+sym = SProxy ∷ SProxy "sym"
+lSlot = HH.slot sym 0 label unit absurd
+rSlot = HH.slot sym 1 magnitude unit absurd
+
+attribute ∷ ∀ m. H.Component HH.HTML (Const Void) Unit Void m
+attribute = component' i r a
+  where
+    i = const { label: "" }
+    a (LabelModify α) = H.modify_ _{ label = α }
+    r = const $ HH.span [class' "attribute"] [lSlot, rSlot]
--- src/Main.purs
+++ src/Main.purs
@@ -1,32 +1,12 @@
 module Main where
 
 import Prelude
-
-import Data.Maybe (Maybe(..))
-import Data.Symbol (SProxy(..))
+import Atom (attribute)
 import Effect (Effect)
-import Effect.Class (class MonadEffect)
-import Effect.Class.Console (logShow)
-import Halogen as H
 import Halogen.Aff as HA
-import Halogen.HTML as HH
-import Halogen.HTML.Properties as HP
-import Halogen.HTML.Events as HE
-import Halogen.VDom.Driver (runUI)
-
-import Prelude
-import Atom (magnitude)
-import Data.Const (Const)
-import Data.Maybe (Maybe(..))
-import Effect (Effect)
-import Halogen as H
-import Halogen.Aff as HA
-import Halogen.HTML as HH
-import Halogen.HTML.Properties as HP
-import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
 
 main ∷ Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  runUI magnitude unit body
+  runUI attribute unit body
