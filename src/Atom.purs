09378e13d24cb46d4813de0287e0deec5d3cfab2
jimd1989 <jamesdalrymple@protonmail.com>
2021-01-27 13:50:10 +1000

    Helpers module

--- src/Atom.purs
+++ src/Atom.purs
@@ -1,33 +1,23 @@
 module Atom where
 
-import Prelude (($), show, (+))
+import Prelude (($), show)
 import Data.Const (Const)
 import Data.Function (const)
+import Data.Int (fromString)
 import Data.Unit (Unit)
 import Data.Void (Void)
 import Halogen as H
-import Halogen.Component as HC
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
-import Halogen.Query.HalogenM as HQ
+import Helpers ((◁), class', component')
 
 type Magnitude = Int
 type MagnitudeState = { magnitude ∷ Magnitude }
-data MagnitudeAction = MagnitudeModify Int
+data MagnitudeAction = MagnitudeModify Magnitude
 
 type Label = String
 data Attribute = Attribute { label ∷ Label, magnitude ∷ Magnitude }
-
-class' ∷ ∀ a b. String → HP.IProp (class ∷ String | b) a
-class' α = HP.class_ $ HH.ClassName α
-
-component' ∷ ∀ a b c d e f g h. 
-  (e → b) → (b → c (HC.ComponentSlot c a g d) d) → (d → HQ.HalogenM b d a h g Unit) → H.Component c f e h g
-component' α β ω = H.mkComponent { initialState, render, eval: H.mkEval H.defaultEval { handleAction = handleAction }}
-  where initialState = α
-        render = β
-        handleAction = ω
 
 magnitude ∷ ∀ m. H.Component HH.HTML (Const Void) Unit Void m
 magnitude = component' i r a
@@ -36,7 +26,8 @@ magnitude = component' i r a
     a (MagnitudeModify n) = H.modify_ _{ magnitude = n }
     r α =
       HH.span [class' "magnitudeBox"]
-       [HH.input [class' "magnitude", HP.placeholder $ show α.magnitude]]
+       [HH.input [class' "magnitude", HP.placeholder $ show α.magnitude, HE.onValueInput parse]]
+    parse = MagnitudeModify ◁ fromString
 
 --label ∷ ∀ w i. String → HH.HTML w i
 --label α = HH.span [HP.class_ $ HH.ClassName "labelBox"] [HH.input [HP.class_ $ HH.ClassName "label"]]
---
+++ src/Helpers.purs
@@ -0,0 +1,57 @@
+module Helpers where
+
+import Prelude (($), compose, map)
+import Control.Apply (apply)
+import Control.Bind (composeKleisliFlipped)
+import Control.Semigroupoid (compose)
+import Data.Const (Const)
+import Data.Function (const)
+import Data.Functor (class Functor, map, mapFlipped)
+import Data.Ord (lessThanOrEq, greaterThanOrEq)
+import Data.Semigroup (append)
+import Data.Unit (Unit)
+import Halogen as H
+import Halogen.Component as HC
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+import Halogen.Query.HalogenM as HQ
+
+class' ∷ ∀ a b. String → HP.IProp (class ∷ String | b) a
+class' α = HP.class_ $ HH.ClassName α
+
+component' ∷ ∀ a b c d e f g h. 
+  (e → b) → (b → c (HC.ComponentSlot c a g d) d) → (d → HQ.HalogenM b d a h g Unit) → H.Component c f e h g
+component' α β ω = H.mkComponent { initialState, render, eval: H.mkEval H.defaultEval { handleAction = handleAction }}
+  where initialState = α
+        render = β
+        handleAction = ω
+
+mapCompose ∷ ∀ a b c f. Functor f ⇒ (a → b) → (c → f a) → c → f b
+mapCompose f = compose (map f)
+
+-- Digraph Dw
+infixr 5 append as ◇
+
+-- Digraph Ob
+infixr 9 compose as ∘
+
+-- Digraph Tl = ◁
+infixr 9 mapCompose as ◁
+
+-- Digraph PL = ◀
+infixr 1 composeKleisliFlipped as ◀
+
+-- Digraph 0.
+infixl 4 map as ⊙
+
+-- dig mr 8854
+infixl 1 mapFlipped as ⊖
+
+-- Digraph 0M
+infixl 4 apply as ●
+
+-- Digraph =<
+infixl 4 lessThanOrEq as ≤
+
+-- Digraph >=
+infixl 4 greaterThanOrEq as ≥
